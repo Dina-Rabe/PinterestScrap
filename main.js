250984865049594a4f621a502df48244a5727879
Dina Rabenarimanitra <dina@allo-win.net>
2024-04-04 18:36:50 +0300

    init2

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("ZAY PORY EH!");
+function fetchImages() {
+    const images = Array.from(document.querySelectorAll('img'));
+    return images.map((img) => img.src);
+  }
+  
+  const imageUrls = fetchImages();
+  console.log(imageUrls);
--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
-document.getElementById("printUrlBtn").addEventListener("click", function () {
-    chrome.extension.getBackgroundPage().handleButtonClick();
-    
+document.getElementById("runButton").addEventListener("click", function () {
+    console.log("TEST");
+    // const keywords = document.getElementById("keywordInput").value.split(";");
+    // keywords.forEach((keyword) => {
+    //   const searchUrl = `https://www.pinterest.fr/search/pins/?q=${encodeURIComponent(keyword.trim())}`;
+    //   chrome.tabs.create({ url: searchUrl });
+    // });
   });
