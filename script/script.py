1104cb906741f88e5f77b78eba8e0ee2659671b5
Dina Rabenarimanitra <dina@allo-win.net>
2024-04-23 18:45:26 +0300

    download script updated

---
+++ script/script.py
@@ -0,0 +1,184 @@
+import urllib.request
+import os
+import logging
+import csv
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from pathlib import Path
+
+class Logging:
+    def __init__(self):
+        # Configure the logger
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        # Create file handlers for different log levels
+        info_handler = logging.FileHandler('info.log')
+        info_handler.setLevel(logging.INFO)
+        error_handler = logging.FileHandler('error.log')
+        error_handler.setLevel(logging.ERROR)
+
+        # Create log formatters
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        info_handler.setFormatter(formatter)
+        error_handler.setFormatter(formatter)
+
+        # Add handlers to the logger
+        self.logger.addHandler(info_handler)
+        self.logger.addHandler(error_handler)
+
+    def log_info(self, message):
+        self.logger.info(message)
+
+    def log_error(self, message):
+        self.logger.error(message)
+
+class PinterestImage:
+    def __init__(self, url_image, url_product=None, image_name=None):
+        self.url_image = url_image
+        self.url_product = url_product
+        self.image_name = image_name
+        self.log = Logging()
+
+    def download_image(self, destination_path):
+        try:
+            # Extract the image filename from the URL
+            image_filename = self.url_image.split('/')[-1]
+
+            # Use custom image name if provided, otherwise use the default image filename
+            if self.image_name:
+                image_filename = self.image_name
+
+            # Construct the full path for the destination file
+            destination_file = os.path.join(destination_path, image_filename)
+
+            # Create the destination path if it doesn't exist
+            os.makedirs(destination_path, exist_ok=True)
+
+            # Download the image
+            urllib.request.urlretrieve(self.url_image, destination_file)
+            self.log.log_info(f"Image {image_filename} successfully downloaded to {destination_file}!")
+            return True
+        except Exception as e:
+            self.log.log_error(str(e))
+            return False
+
+class AppController:
+    def __init__(self, file_name, destination_path=None):
+        self.file_name = file_name
+        self.destination_path = os.path.join(destination_path, os.path.splitext(os.path.basename(file_name))[0]) #Fetch the file name only without extension 
+        self.log = Logging()
+        
+    def isFileValid(self):
+        try:
+            with open(self.file_name, 'r') as file:
+                # Check file extension
+                if not self.file_name.lower().endswith('.csv'):
+                    self.log.log_error(f"ERROR, {self.file_name} is not a CSV file")
+                    return False
+
+                # Read CSV file
+                csv_reader = csv.reader(file)
+                header = next(csv_reader)  # Get the header
+                column_count = len(header)
+
+                # Validate CSV format and column count
+                if column_count != 2:
+                    self.log.log_error(f"ERROR column count greater than 2!")
+                    return False
+
+                # for index, row in enumerate(csv_reader):
+                #     if len(row) != column_count:
+                #         self.log.log_error(f"ERROR at {index} line of file {self.file_name}, download aborted!")
+                #         return False
+
+            return True
+
+        except Exception as e:
+            self.log.log_error(f"Error occurred while validating the file: {str(e)}")
+            return False
+
+    def process(self):
+        pinterest_images = []
+
+        try:
+            with open(self.file_name, 'r') as file:
+                csv_reader = csv.reader(file)
+                next(csv_reader)  # Skip the header
+
+                for index, row in enumerate(csv_reader):
+                    url_image = row[0]
+                    url_product = row[1]
+                    image_name = f"{index}.jpg"
+
+                    pinterest_image = PinterestImage(url_image, url_product, image_name)
+                    if pinterest_image.download_image(self.destination_path):
+                        pinterest_images.append(pinterest_image)
+
+        except Exception as e:
+            self.log.log_error(f"Error occurred while processing the file: {str(e)}")
+
+        return pinterest_images
+
+class FileUploader:
+    def __init__(self, root):
+        self.root = root
+        self.file_names = []
+        self.folder_destination = ""
+
+        self.root.title("File Uploader")
+
+        self.file_listbox = tk.Listbox(self.root, selectmode=tk.MULTIPLE, width=40)
+        self.file_listbox.pack(pady=10)
+
+        self.browse_files_button = tk.Button(self.root, text="Browse Files", command=self.browse_files)
+        self.browse_files_button.pack(pady=5)
+
+        self.folder_frame = tk.Frame(self.root)
+        self.folder_frame.pack(pady=5)
+
+        self.folder_label = tk.Label(self.folder_frame, text="Destination Path:")
+        self.folder_label.pack(side=tk.LEFT)
+
+        self.folder_entry = tk.Entry(self.folder_frame, width=30)
+        self.folder_entry.pack(side=tk.LEFT)
+
+        self.browse_folder_button = tk.Button(self.root, text="Select Destination", command=self.browse_folder)
+        self.browse_folder_button.pack(pady=5)
+
+        self.upload_button = tk.Button(self.root, text="Download all files", command=self.download_files)
+        self.upload_button.pack(pady=5)
+
+    def browse_files(self):
+        home_directory = str(Path.home())
+        file_paths = filedialog.askopenfilenames(initialdir=home_directory, filetypes=[("CSV files", "*.csv")])
+        self.file_names = list(file_paths)
+        self.update_file_listbox()
+
+    def browse_folder(self):
+        home_directory = str(Path.home())
+        folder_path = filedialog.askdirectory(initialdir=home_directory)
+        self.folder_destination = folder_path
+        self.folder_entry.delete(0, tk.END)
+        self.folder_entry.insert(tk.END, self.folder_destination)
+
+    def download_files(self):
+        if len(self.file_names) > 0:
+            for file_name in self.file_names:
+                app_controller = AppController(file_name,self.folder_destination)
+                if app_controller.isFileValid():
+                    app_controller.process()
+                else:
+                    messagebox.showerror("ERROR", f"File {file_name} is not valid!")
+        else:
+            print("No files selected.")
+
+    def update_file_listbox(self):
+        self.file_listbox.delete(0, tk.END)
+        for file_name in self.file_names:
+            self.file_listbox.insert(tk.END, file_name)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    file_uploader = FileUploader(root)
+    root.mainloop()
